ef095ed2ebfca1f7be160303fdcc90401efce78d
Byron Llerena <byronllerena@MacBook-Pro-de-Byron.local>
2015-09-25 17:33:22 +0100

    So, the program is running fine, this I created EditChat.java which reads a folder and takes all conversations, removing the info, lines with emails, dates, hours , numbers, so that I get pure text. After removing all that it creates two corporas, once with sentences and one with words, before counting ngrams or anything lets first focus on what the algorithms need.

--- src/Whatsapp/EditChat.java
+++ src/Whatsapp/EditChat.java
@@ -6,13 +6,17 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EditChat {
 	
-	public final static String nameCorpora="corpusWhatsapp.txt";
+	private final String nameSentenceCorpora="corpusSentencesWhatsapp.txt";
+	private final String nameWordCorpora="corpusWordWhatsapp.txt";
 	private String pathFolder="";
+	private PrintWriter writerWordCorpus;
+	private PrintWriter writerSentenceCorpus;
 	
 	public void runEdition(String Uri){
 		//Uri is the folder where all conversations are stored
@@ -24,7 +28,19 @@ public class EditChat {
 		String lineEddited="";
 		
 		try {
-			PrintWriter writer=new PrintWriter(pathFolder+nameCorpora);
+			this.writerSentenceCorpus=new PrintWriter(this.pathFolder+this.nameSentenceCorpora,"UTF-8");
+			this.writerWordCorpus=new PrintWriter(this.pathFolder+this.nameWordCorpora,"UTF-8");
+				
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			
 			
 			for(String nameFile:files){
 				
@@ -37,15 +53,38 @@ public class EditChat {
 					
 					//Filtrate only sentences with more than a word
 					
+					if(isValid(lineEddited)){
+						
+						lineEddited=removeSymbols(lineEddited);
+						lineEddited=removeEmojis(lineEddited);
+						lineEddited=removeExtraWhiteSpaces(lineEddited);
+						lineEddited=removeFirstWhiteSpace(lineEddited);
+						
+						
+						if(isUsefulData(lineEddited)){
+							String[] lines=splitLineInCapital(lineEddited);
+							
+							for(int i=0;i<lines.length;i++){
+								sentenceCorpora(lines[i]);
+								wordCorpora(lines[i]);
+									
+							}
+							
+							
+						}
+					}
 					
 					
 					
 					
 				}
-				
+				reader.close();
 				
 				
 			}
+			
+			this.writerSentenceCorpus.close();
+			this.writerWordCorpus.close();
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -53,6 +92,18 @@ public class EditChat {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}finally{
+			
+			try{
+				if(reader!=null){
+					
+					reader.close();
+				}
+				
+			}catch(IOException e){
+				
+				
+			}
 		}
 		
 		
@@ -64,37 +115,188 @@ public class EditChat {
 	
 	private String removeInfo(String line){
 		
-		if (hasInfo(line)){
+		/*There are different type of hours and date formats at the begining of each
+		 * sentecne which is why after analyzing them these are the formats found and their
+		 * different combinations:
+		 *   
+		 *   DATES
+		 *   =====
+		 *   
+		 *   1D) Date type 1: 24/7/15 
+		 *   in format dd/mm/year, excluding the zero number for months and days
+		 *   and the 20 in 2015
+		 *   
+		 *   2D) Date type 2: 07/08/2015,
+		 *   in the format dd/mm/year it makes use of the number zero
+		 *   and besides of using 20 in 2015 it ends with a comma
+		 *   
+		 *   3D) Date type 3: 09/14/15 
+		 *   is the same as number 2 but it does not use the comma
+		 *   
+		 *   TIMES
+		 *   =====
+		 *   
+		 *   1T) Time type 1: 14:18:46:
+		 *                     8:39:39:
+		 *       it does not make use of the zero number in the hours
+		 *       but it does in the minutes and seconds
+		 *   
+		 *   2T)  9:29 AM - 
+		 *       12:27 PM -  
+		 *       
+		 *   3T) 11:37 -
+		 *        9:45 -
+		 *        
+		 *   4T) 8:52 PM: 
+		 *   
+		 *   COMBINATIONS
+		 *   ============
+		 *   
+		 *   1D+1T
+		 *   2D+2T
+		 *   2D+3T
+		 *   3D+4T
+		 *   
+		 *   Based on this combinations 4 regex are created
+		 *   regex1=>1d+1H,3D+4H
+		 *   regex2=>2D+2H,2D+3h
+		 *   
+		 *   
+		 *   
+		 *   */
+		
+			String regex1="(\\w+/\\w+/\\w+)\\s(\\w+:\\w+).+:";
+			String regex2="(\\w+/\\w+/\\w+),\\s(\\w+:\\w+).+:";
+			String regexComplete=regex1+"|"+regex2;
 			
-			//int index=line.
+			Matcher matcheador=Pattern.compile(regexComplete).matcher(line);
+			
+			
+		if (matcheador.find()){
+			//If the line has info it will remove it, otherwise it will not edit it at all
+			
+			return matcheador.replaceAll("");
 			
 		}else{
 			
-			
 			return line;
-		}
-			
+		}	
 			
 		
 		
-		return null;
+		 
+		
+	}
+   
+	private boolean isValid(String line){
+		
+		//It returns true if any number is present or email , website,etc.. It is done because if we take this off sentences tend to loose syntax 
+		
+		String newLine=null;
+		String regex="[\\S]+://[\\S]+|www[\\S]+com|<[\\S]+[\\s]+[\\S]+>|[\\S]+@[\\S]+.com|[0123456789]";
+		
+		Pattern patron=Pattern.compile(regex);
+		Matcher matcheador=patron.matcher(line);
+		
+		return !matcheador.find();
+	}
+	
+	private String removeSymbols(String line){
+	
+		
+		return line.replaceAll("[^\\p{L}\\p{Z}]","");
 		
 	}
 	
-	private boolean hasInfo(String line){
-		
-		String regexDate="";
-		String regexTime="";
-		Pattern patternDate=Pattern.compile(regexDate);
-		Pattern patternTime=Pattern.compile(regexTime);
-		
-		String[] words=line.split("\\s");
-		Matcher matchedorDate=patternDate.matcher(words[0]);
-		Matcher matcheadorTime=patternTime.matcher(words[1]);
+   private String removeEmojis(String line){
 		
 		
-		return matcheadorTime.find()&matchedorDate.find();
+		byte[] utf8=null;
+		String line_utf8=null;
+		
+		try {
+		   utf8 = line.getBytes("UTF-8");
+		   line_utf8 = new String(utf8,"UTF-8");
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		};
+	    
+		
+	    String regex="[\uD83C-\uDBFF\uDC00-\uDFFF]+";   
+	    Pattern pattern=Pattern.compile(regex);	  
+	    Matcher matcheador=pattern.matcher(line_utf8);
+	    
+	    return matcheador.replaceAll("");
+	
 		
 	}
+	
+   private String removeFirstWhiteSpace(String line){
+		
+		
+		Pattern patron=Pattern.compile("^[\\s]+");
+		Matcher matcheador=patron.matcher(line);
+		
+		return matcheador.replaceAll("");
+		
+	
+	}
+	
+	private String removeExtraWhiteSpaces(String line){
+		
+		Pattern patron=Pattern.compile("[\\s]{2,}");
+		Matcher matcheador=patron.matcher(line);
+		
+		return matcheador.replaceAll(" ");
+		
+	}
+	
+	private String[] splitLineInCapital(String line){
+		
+		String regex="\\s+(?=[A-Z])";
+        String[] lines=line.split(regex);
+
+		return lines;
+	}
+	
+	private boolean isUsefulData(String line){
+		/*It is a useful data if its length is > 1 and it is more than a word
+		 * */
+		
+	 return line.split("\\s").length>1&&line.length()>1;
+	}
+	
+	private void sentenceCorpora(String line){
+		
+	
+			this.writerSentenceCorpus.println(line.toLowerCase());
+			
+	}
+	
+	private void closeWriterSentence(){
+		
+		this.writerSentenceCorpus.close();
+	}
+	
+	private void closeWriterWord(){
+		this.writerWordCorpus.close();
+	}
+	
+	private void wordCorpora(String line){
+		
+		String[] words=line.split("\\s");
+		
+					
+		for(String word:words){
+		
+			this.writerWordCorpus.println(word);
+			 
+			
+		}
+		this.writerWordCorpus.println();
+		
+	}
+	
 
 }
